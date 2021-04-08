ae6b042327d7426d510738e3626084033610fedf
B4D-1D34 <rapkor4@gmail.com>
2021-04-09 03:45:23 +0500

    transformed all page layout to grid, slider now works

---
+++ index.js
@@ -0,0 +1,35 @@
+const leftArrow = document.getElementById("left-arrow");
+const rightArrow = document.getElementById("right-arrow");
+const sliderWrapper = document.getElementById("slider");
+
+const getCardItemDimensions = () => {
+  const advantagesItem = document.getElementsByClassName(
+    "gallery__wrapper--img"
+  )[0];
+  const advantagesItemStyles = window.getComputedStyle(advantagesItem);
+  const itemOffset =
+    Number(advantagesItemStyles.marginRight.replace(/[^-\d.]/g, "")) +
+    Number(advantagesItemStyles.width.replace(/[^-\d.]/g, ""));
+
+  return itemOffset;
+};
+
+const itemOffset = getCardItemDimensions();
+
+let position = 0;
+
+rightArrow.addEventListener("click", () => {
+  //   if (position === 1) return;
+  const coord = sliderWrapper.style.transform.replace(/[^-\d.]/g, "");
+  sliderWrapper.style.transform = `translateX(${Number(coord) - itemOffset}px)`;
+
+  position++;
+});
+
+leftArrow.addEventListener("click", () => {
+  if (position === 0) return;
+  const coord = sliderWrapper.style.transform.replace(/[^-\d.]/g, "");
+  sliderWrapper.style.transform = `translateX(${Number(coord) + itemOffset}px)`;
+
+  position--;
+});
